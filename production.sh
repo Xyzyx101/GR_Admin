6042b3cd7bcf1d9da7dbf5c64afff00171d9b214
Andrew Perrault <andrew.perrault@yahoo.ca>
2018-02-25 10:43:30 -0500

    Fixed the branch checkout scripts

--- production.sh
+++ production.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# This will switch to the production branch and automatically checkout the correct version of the grFirebase module
+
 git checkout production
-git submodule update --remote grFirebase
+git submodule update --remote app/grFirebase
--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# This will switch to the test branch and automatically checkout the correct version of the grFirebase module
+
 git checkout master
-git submodule update --remote grFirebase
+git submodule update --remote app/grFirebase
