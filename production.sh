c2c6cb2bde1dbf1cb3654fa025ab1181fa6f25e0
Andrew <andrew.perrault@yahoo.ca>
2018-02-28 16:47:49 -0500

    Added irebase hosting config

--- production.sh
+++ production.sh
@@ -4,3 +4,10 @@
 
 git checkout production
 git submodule update --remote app/grFirebase
+
+#switch to correct firebase target
+if hash firebase 2>/dev/null; then
+	firebase use production
+else
+	echo "Firebase cli not found.  Aborting firebase target switch"
+fi
